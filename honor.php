18da3bc608dab13f6e0c0fca9b59a78454e6f812
xiongguoy <xiongguoy@6e048d84-7432-0410-813b-84b186a6e9d5>
2009-08-03 04:05:20 +0000

    Honor Page now allows to view by highest rank/honor points, kills or arena points. as per requested by mikebeck
    honestly this page is now just like top100, just a few fields left out.

--- honor.php
+++ honor.php
@@ -19,21 +19,19 @@
  $sql->connect($characters_db[$realm_id]['addr'], $characters_db[$realm_id]['user'], $characters_db[$realm_id]['pass'], $characters_db[$realm_id]['name']);
 
  $start = (isset($_GET['start'])) ? $sql->quote_smart($_GET['start']) : 0;
- $order_by = (isset($_GET['order_by'])) ? $sql->quote_smart($_GET['order_by']) : "atid";
-
- $dir = (isset($_GET['dir'])) ? $sql->quote_smart($_GET['dir']) : 1;
- $order_dir = ($dir) ? "ASC" : "DESC";
- $dir = ($dir) ? 0 : 1;
+ $order_by = (isset($_GET['order_by'])) ? $sql->quote_smart($_GET['order_by']) :"honor";
 
  $query = $sql->query("SELECT
 				guid,name,race,class,
 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_POINTS+1)."), ' ', -1) AS UNSIGNED) AS honor ,
+				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_KILL+1)."), ' ', -1) AS UNSIGNED) AS kills,
 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_LEVEL+1)."), ' ', -1) AS UNSIGNED) AS level,
+				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`data`, ' ', ".(CHAR_DATA_OFFSET_ARENA_POINTS+1)."), ' ', -1) AS UNSIGNED) AS arena,
 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_GUILD_ID+1)."), ' ', -1) AS UNSIGNED) as GNAME,
 				mid(lpad( hex( CAST(substring_index(substring_index(data,' ',".(CHAR_DATA_OFFSET_GENDER+1)."),' ',-1) as unsigned) ),8,'0'),4,1) as gender
 				FROM `characters`
 				where race in (1,3,4,7,11)
-				order by CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_POINTS+1)."), ' ', -1) AS UNSIGNED) desc
+				ORDER BY $order_by DESC
 				LIMIT 25;");
 
  $this_page = $sql->num_rows($query);
@@ -57,8 +55,10 @@ $output .= "<script type=\"text/javascript\">
     <th width=\"7%\">{$lang_honor['race']}</th>
     <th width=\"7%\">{$lang_honor['class']}</th>
     <th width=\"7%\">{$lang_honor['level']}</th>
-    <th width=\"10%\">{$lang_honor['honor points']}</th>
-    <th width=\"9%\">{$lang_honor['honor']}</th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=honor\"".($order_by=='honor' ? " class=DESC" : "").">{$lang_honor['honor']}</a></th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=honor\"".($order_by=='honor' ? " class=DESC" : "").">{$lang_honor['honor points']}</a></th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=kills\"".($order_by=='kills' ? " class=DESC" : "").">Kills</a></th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=arena\"".($order_by=='arena' ? " class=DESC" : "").">AP</a></th>
     <th width=\"30%\">{$lang_honor['guild']}</th>
   </tr>";
 
@@ -67,7 +67,7 @@ while ($char = $sql->fetch_row($query))	{
 
 $guild_name = $sql->fetch_row($sql->query("SELECT `name` FROM `guild` WHERE `guildid`=".$char[6].";"));
 
-    $level = $char[5];
+    $level = $char[6];
 
     if($level < 10)
       $lev = '<font color="#FFFFFF">'.$level.'</font>';
@@ -90,12 +90,14 @@ $guild_name = $sql->fetch_row($sql->query("SELECT `name` FROM `guild` WHERE `gui
 
   	$output .= " <tr>
 			 <td><a href=\"char.php?id=$char[0]\">".htmlentities($char[1])."</a></td>
-		 	 <td><img src='img/c_icons/{$char[2]}-{$char[7]}.gif' onmousemove='toolTip(\"".get_player_race($char[2])."\",\"item_tooltip\")' onmouseout='toolTip()'></td>
+		 	 <td><img src='img/c_icons/{$char[2]}-{$char[9]}.gif' onmousemove='toolTip(\"".get_player_race($char[2])."\",\"item_tooltip\")' onmouseout='toolTip()'></td>
 		  	 <td><img src='img/c_icons/{$char[3]}.gif' onmousemove='toolTip(\"".get_player_class($char[3])."\",\"item_tooltip\")' onmouseout='toolTip()'></td>
 			 <td>$lev</td>
+			 <td><span onmouseover='toolTip(\"".$CHAR_RANK[$CHAR_RACE[$char[2]][1]][pvp_ranks($char[4])]."\",\"item_tooltip\")' onmouseout='toolTip()' style='color: white;'><img src='img/ranks/rank".pvp_ranks($char[4],$CHAR_RACE[$char[2]][1]).".gif'></span></td>
 			 <td>$char[4]</td>
-		     <td><span onmouseover='toolTip(\"".$CHAR_RANK[$CHAR_RACE[$char[2]][1]][pvp_ranks($char[4])]."\",\"item_tooltip\")' onmouseout='toolTip()' style='color: white;'><img src='img/ranks/rank".pvp_ranks($char[4],$CHAR_RACE[$char[2]][1]).".gif'></span></td>
-			 <td><a href=\"guild.php?action=view_guild&amp;error=3&amp;id=$char[6]\">".htmlentities($guild_name[0])."</a></td>
+			 <td>$char[5]</td>
+			 <td>$char[7]</td>
+			 <td><a href=\"guild.php?action=view_guild&amp;error=3&amp;id=$char[8]\">".htmlentities($guild_name[0])."</a></td>
  			 </tr>";
 }
 
@@ -103,13 +105,15 @@ $output .= "</table><br /></fieldset>";
 
 $query = $sql->query("SELECT
  				guid,name,race,class,
- 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_POINTS+1)."), ' ', -1) AS UNSIGNED) AS highest_rank ,
+ 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_POINTS+1)."), ' ', -1) AS UNSIGNED) AS honor ,
+				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_KILL+1)."), ' ', -1) AS UNSIGNED) AS kills,
  				CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_LEVEL+1)."), ' ', -1) AS UNSIGNED) AS level,
+ 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`data`, ' ', ".(CHAR_DATA_OFFSET_ARENA_POINTS+1)."), ' ', -1) AS UNSIGNED) AS arena,
 				CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`characters`.`data`, ' ', ".(CHAR_DATA_OFFSET_GUILD_ID+1)."), ' ', -1) AS UNSIGNED) as GNAME,
 				mid(lpad( hex( CAST(substring_index(substring_index(data,' ',".(CHAR_DATA_OFFSET_GENDER+1)."),' ',-1) as unsigned) ),8,'0'),4,1) as gender
 				FROM `characters`
 				where race not in (1,3,4,7,11)
-				order by CAST( SUBSTRING_INDEX(SUBSTRING_INDEX(`data`, ' ', ".(CHAR_DATA_OFFSET_HONOR_POINTS+1)."), ' ', -1) AS UNSIGNED) desc
+				ORDER BY $order_by DESC
 				LIMIT 25;");
 
 
@@ -132,8 +136,10 @@ $output .= "<script type=\"text/javascript\">
     <th width=\"7%\">{$lang_honor['race']}</th>
     <th width=\"7%\">{$lang_honor['class']}</th>
     <th width=\"7%\">{$lang_honor['level']}</th>
-    <th width=\"10%\">{$lang_honor['honor points']}</th>
-    <th width=\"9%\">{$lang_honor['honor']}</th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=honor\"".($order_by=='honor' ? " class=DESC" : "").">{$lang_honor['honor']}</a></th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=honor\"".($order_by=='honor' ? " class=DESC" : "").">{$lang_honor['honor points']}</a></th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=kills\"".($order_by=='kills' ? " class=DESC" : "").">Kills</a></th>
+	<th width=\"5%\"><a href=\"honor.php?order_by=arena\"".($order_by=='arena' ? " class=DESC" : "").">AP</a></th>
     <th width=\"30%\">{$lang_honor['guild']}</th>
   </tr>";
 
@@ -142,7 +148,7 @@ while ($char = $sql->fetch_row($query))	{
 
 $guild_name = $sql->fetch_row($sql->query("SELECT `name` FROM `guild` WHERE `guildid`=".$char[6].";"));
 
-    $level = $char[5];
+    $level = $char[6];
 
     if($level < 10)
       $lev = '<font color="#FFFFFF">'.$level.'</font>';
@@ -165,12 +171,14 @@ $guild_name = $sql->fetch_row($sql->query("SELECT `name` FROM `guild` WHERE `gui
 
   	$output .= " <tr>
 			 <td><a href=\"char.php?id=$char[0]\">".htmlentities($char[1])."</a></td>
-		 	 <td><img src='img/c_icons/{$char[2]}-{$char[7]}.gif' onmousemove='toolTip(\"".get_player_race($char[2])."\",\"item_tooltip\")' onmouseout='toolTip()'></td>
+		 	 <td><img src='img/c_icons/{$char[2]}-{$char[9]}.gif' onmousemove='toolTip(\"".get_player_race($char[2])."\",\"item_tooltip\")' onmouseout='toolTip()'></td>
 		  	 <td><img src='img/c_icons/{$char[3]}.gif' onmousemove='toolTip(\"".get_player_class($char[3])."\",\"item_tooltip\")' onmouseout='toolTip()'></td>
 			 <td>$lev</td>
-			 <td>$char[4]</td>
 		     <td><span onmouseover='toolTip(\"".$CHAR_RANK[$CHAR_RACE[$char[2]][1]][pvp_ranks($char[4])]."\",\"item_tooltip\")' onmouseout='toolTip()' style='color: white;'><img src='img/ranks/rank".pvp_ranks($char[4],$CHAR_RACE[$char[2]][1]).".gif'></span></td>
-			 <td><a href=\"guild.php?action=view_guild&amp;error=3&amp;id=$char[6]\">".htmlentities($guild_name[0])."</a></td>
+			 <td>$char[4]</td>
+			 <td>$char[5]</td>
+			 <td>$char[7]</td>
+			 <td><a href=\"guild.php?action=view_guild&amp;error=3&amp;id=$char[8]\">".htmlentities($guild_name[0])."</a></td>
 			 </tr>";
 }
 
