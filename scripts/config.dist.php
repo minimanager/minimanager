6b6912b6263ebbc515166bb1e9950829671e0ea7
brunogcar <brunogcar@6e048d84-7432-0410-813b-84b186a6e9d5>
2009-10-06 18:43:51 +0000

    Bump for 0.15 development.

--- scripts/config.dist.php
+++ scripts/config.dist.php
@@ -21,7 +21,7 @@
 //---- Version Information ----
 
 $show_version['show']        =  '1';    // 0 - Don't Show, 1 - Show Version, 2 - Show Version and SVN Revision
-$show_version['version']     =  '0.14';
+$show_version['version']     =  '0.15';
 $show_version['version_lvl'] = '-1';    // Minimum account level to show Version to, -1 is guest account
 $show_version['svnrev']      =  '0';    // SVN Revision will be read from .svn folder, values here hold no meaning or effect
 $show_version['svnrev_lvl']  =  '5';    // Minimum account level to show SVN Revision to.
