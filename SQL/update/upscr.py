bdf5490cbf445e31d9fd5cb30946dbe73c06aba1
coderasm <coderasm@gmail.com>
2010-10-26 17:55:32 -0700

    brunosrc

--- SQL/update/upscr.py
+++ SQL/update/upscr.py
@@ -6,4 +6,4 @@ patches = sorted(patches)
 for x in patches:
   #db = x.split("_")[2].replace('.sql', '')
   db = 'mmfpm'
-  os.system("mysql -u mangos -pLi3bnitz95k -v " + db + " < " + x)
+  os.system("mysql -u user -ppassword -v " + db + " < " + x)
